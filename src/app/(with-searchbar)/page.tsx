dcb6af8ab168cd0ffb555f3e2bc85de7359451a4
suhyun-kim9 <ksh020674@gmail.com>
2025-07-23 17:36:20 +0900

    페이지/폴더구조 세팅

---
+++ src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,50 @@
+import MovieItem from "@/components/movie-item";
+import style from "./page.module.css";
+import { MovieData } from "@/types";
+
+async function AllMovies() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const allMovies: MovieData[] = await response.json();
+
+  return (
+    <div>
+      {allMovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoMovies() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const recoMovies: MovieData[] = await response.json();
+
+  return (
+    <div>
+      {recoMovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 영화</h3>
+        <RecoMovies />
+      </section>
+      <section>
+        <h3>등록된 모든 영화</h3>
+        <AllMovies />
+      </section>
+    </div>
+  );
+}
---
+++ src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,19 @@
+import MovieItem from "@/components/movie-item";
+import { MovieData } from "@/types";
+
+export default async function Page({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
+  const { q } = await searchParams;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const movies: MovieData[] = await response.json();
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
