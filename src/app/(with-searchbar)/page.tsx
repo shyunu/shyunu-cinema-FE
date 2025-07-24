eb91d310be9ec85d4071af03680a2c87a3139661
suhyun-kim9 <ksh020674@gmail.com>
2025-07-24 15:38:36 +0900

    페이지 설정

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -3,7 +3,7 @@ import style from "./page.module.css";
 import { MovieData } from "@/types";
 
 async function AllMovies() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`, { cache: "force-cache" });
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
   }
@@ -19,7 +19,7 @@ async function AllMovies() {
 }
 
 async function RecoMovies() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`, { next: { revalidate: 3 } });
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
   }
