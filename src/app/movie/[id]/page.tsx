4d951bb902f4d24e965d501ecbf66a7c8ae6a70b
suhyun-kim9 <ksh020674@gmail.com>
2025-07-24 18:32:07 +0900

    서버액션 추가

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -5,9 +5,8 @@ export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
 
-export default async function Page({ params }: { params: Promise<{ id: string | string[] }> }) {
-  const { id } = await params;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/${id}`);
+async function MovieDetail({ movieId }: { movieId: string }) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/${movieId}`);
   if (!response.ok) {
     if (response.status === 404) {
       notFound();
@@ -19,7 +18,7 @@ export default async function Page({ params }: { params: Promise<{ id: string | 
   const { title, subTitle, description, releaseDate, company, genres, runtime, posterImgUrl } = movie;
 
   return (
-    <div className={style.container}>
+    <section>
       <div className={style.cover_img_container} style={{ backgroundImage: `url('${posterImgUrl}')` }}>
         <img src={posterImgUrl} />
       </div>
@@ -29,6 +28,38 @@ export default async function Page({ params }: { params: Promise<{ id: string | 
         {genres} | {runtime}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+function ReviewEditor() {
+  async function createReviewAction(formData: FormData) {
+    "use server";
+
+    const content = formData.get("content")?.toString();
+    const author = formData.get("author")?.toString();
+
+    console.log(content, author);
+  }
+
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input name="content" placeholder="리뷰 내용" />
+        <input name="author" placeholder="작성자" />
+        <button type="submit">작성하기</button>
+      </form>
+    </section>
+  );
+}
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+
+  return (
+    <div className={style.container}>
+      <MovieDetail movieId={id} />
+      <ReviewEditor />
     </div>
   );
 }
