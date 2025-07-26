426c1a064501e783f12a82d5d3b73af25f9c4da4
suhyun-kim9 <ksh020674@gmail.com>
2025-07-26 15:02:31 +0900

    리뷰 중복 등록 방지 구현

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -2,13 +2,16 @@
 
 import { revalidatePath, revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const movieId = formData.get("movieId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!movieId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰내용과 작성자를 입력해주세요",
+    };
   }
 
   try {
@@ -16,10 +19,18 @@ export async function createReviewAction(formData: FormData) {
       method: "POST",
       body: JSON.stringify({ movieId, content, author }),
     });
-    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     revalidateTag(`review-${movieId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
--- src/components/review-editor.tsx
+++ src/components/review-editor.tsx
@@ -1,16 +1,29 @@
+"use client";
+
 import style from "./review-editor.module.css";
 import { createReviewAction } from "@/actions/create-review.action";
 import Button from "@mui/material/Button";
+import { useActionState, useEffect } from "react";
 
 export default function ReviewEditor({ movieId }: { movieId: string }) {
+  const [state, formAction, isPending] = useActionState(createReviewAction, null);
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form className={style.form_container} action={createReviewAction}>
+      <form className={style.form_container} action={formAction}>
         <input name="movieId" value={movieId} hidden readOnly />
-        <textarea required name="content" placeholder="리뷰 내용" />
+        <textarea disabled={isPending} required name="content" placeholder="리뷰 내용" />
         <div className={style.submit_container}>
-          <input required name="author" placeholder="작성자" />
-          <Button type="submit">작성하기</Button>
+          <input disabled={isPending} required name="author" placeholder="작성자" />
+          <Button disabled={isPending} type="submit">
+            {isPending ? "..." : "작성하기"}
+          </Button>
         </div>
       </form>
     </section>
