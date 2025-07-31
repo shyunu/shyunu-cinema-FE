15bb55d7038d5f0081c54a85976890174bd60e28
suhyun-kim9 <ksh020674@gmail.com>
2025-07-31 16:46:48 +0900

    컴포넌트 분리

---
+++ src/components/home/AllMovies.tsx
@@ -0,0 +1,22 @@
+import { MovieData } from "@/types";
+import { delay } from "@/util/delay";
+import MovieItem from "../movie-item";
+
+export default async function AllMovies() {
+  await delay(1500);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`, {
+    cache: "force-cache",
+  });
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const allMovies: MovieData[] = await response.json();
+
+  return (
+    <div>
+      {allMovies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+}
